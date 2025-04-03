bb6e3a3f2d8271907712c10100c128c1a3f139a9
Benel levy <benel.levy@weizmann.ac.il>
2025-04-03 18:56:21 +0300

    add homework for TA

---
+++ day2/circle.py
@@ -0,0 +1,5 @@
+radius = float(input("what is the radius of the circle? "))
+area = 3.14 * radius * radius
+circumference = 2 * 3.14 * radius   
+print("The area of the circle is: ", area)
+print("The circumference of the circle is: ", circumference)
---
+++ day2/rectangle.py
@@ -0,0 +1,6 @@
+high = float(input("Enter the height of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = high * width
+perimeter = 2 * (high + width)
+print("The area of the rectangle is: ", area)   
+print("The perimeter of the rectangle is: ", perimeter)
